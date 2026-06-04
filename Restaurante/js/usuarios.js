dee2156e0d6b09fb754c8ca15d4015e411c82e89
Mattias Vargas <mattiasvargas27@gmail.com>
2026-06-04 16:56:25 -0500

    validate_module_user:Add validations to inputs

--- Restaurante/js/usuarios.js
+++ Restaurante/js/usuarios.js
@@ -15,4 +15,36 @@ let telefono_usuarios=document.getElementById("telefono_usuarios").value;
 let email=document.getElementById("email").value;
 let genero=document.getElementById("genero").value;
 let cargo=document.getElementById("cargo").value;
+let password = document.getElementById("password").value;
+let fecha_nacimiento = document.getElementById("fecha_nacimiento").value;
 
+
+
+if (nombre_usuarios== "" ||apellido== "" ||documento== "" ||telefono_usuarios== "" ||email== "" ||cargo== "" || password== "" ){
+    console.log("Los campos no pueden estar vacios")
+}
+else{
+    if(nombre_usuarios != /[a-zA-Z]/){
+        console.log("Los datos son incorrectos")
+    }
+    if(apellido != /[a-zA-Z]/){
+        console.log("Los datos son incorrectos")
+    }
+
+    if(documento.length() >=10){
+        console.log("Los datos son incorrectos")
+    }
+    if(telefono_usuarios.length() >=10){
+        console.log("Los datos son incorrectos")
+    }
+    if(email != /[@]/){
+        console.log("Los datos son incorrectos")
+    }
+    if(cargo != /[a-zA-Z]/){
+        console.log("Los datos son incorrectos")
+    }
+    if(cargo != /[a-zA-Z]/){
+        console.log("Los datos son incorrectos")
+    }
+    
+}
